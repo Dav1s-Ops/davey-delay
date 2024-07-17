a8388cedaf0de6c502003634a0c1f98ce77e17f3
Davis Weimer <davisweimer@gmail.com>
2024-07-17 14:29:21 -0600

    Add Parameters constructor and createParameterLayout to Parameters.cpp

--- Source/Parameters.cpp
+++ Source/Parameters.cpp
@@ -9,3 +9,24 @@
 */
 
 #include "Parameters.h"
+
+Parameters::Parameters(juce::AudioProcessorValueTreeState& apvts)
+{
+    auto* param = apvts.getParameter(gainParamID.getParamID());
+    gainParam = dynamic_cast<juce::AudioParameterFloat*>(param);
+}
+
+juce::AudioProcessorValueTreeState::ParameterLayout Parameters::createParameterLayout()
+{
+    juce::AudioProcessorValueTreeState::ParameterLayout layout;
+    
+    layout.add(std::make_unique<juce::AudioParameterFloat>(gainParamID,
+                                                           // Name of the parameter
+                                                           "Output Gain",
+                                                           // Value range
+                                                           juce::NormalisableRange<float> { -12.0f, 12.0f },
+                                                           // Default value for range
+                                                           0.0f));
+    return layout;
+}
+
