c83ad27ef83e529f79b9b0434b9a56f4a50b28ae
Davis Weimer <davisweimer@gmail.com>
2024-07-22 10:46:10 -0600

    Move delayLine.setDelay into sample for loop

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -140,14 +140,15 @@ void DelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, [[mayb
     params.update();
     
     float sampleRate = float(getSampleRate());
-    float delayInSamples = params.delayTime / 1000.0f * sampleRate;
-    delayLine.setDelay(delayInSamples);
     
     float* channelDataL = buffer.getWritePointer(0);
     float* channelDataR = buffer.getWritePointer(1);
     
     for (int sample = 0; sample < buffer.getNumSamples(); ++sample) {
         params.smoothen();
+        
+        float delayInSamples = params.delayTime / 1000.0f * sampleRate;
+        delayLine.setDelay(delayInSamples);
         
         float dryL = channelDataL[sample];
         float dryR = channelDataR[sample];
@@ -160,6 +161,7 @@ void DelayAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, [[mayb
         
         channelDataL[sample] = (dryL + wetL) * params.gain;
         channelDataR[sample] = (dryR + wetR) * params.gain;
+
     }
 
 /*
