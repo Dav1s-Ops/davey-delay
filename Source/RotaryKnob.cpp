1cf878918809935a45868f1f4d89f3dce99feece
Davis Weimer <davisweimer@gmail.com>
2024-07-23 11:29:26 -0600

    Setup member init list and add gain knob to constructor

--- Source/RotaryKnob.cpp
+++ Source/RotaryKnob.cpp
@@ -12,10 +12,23 @@
 #include "RotaryKnob.h"
 
 //==============================================================================
-RotaryKnob::RotaryKnob()
+RotaryKnob::RotaryKnob(const juce::String& text,
+                       juce::AudioProcessorValueTreeState& apvts,
+                       const juce::ParameterID& parameterID)
+    : attachment(apvts, parameterID.getParamID(), slider)
 {
-    // In your constructor, you should add any child components, and
-    // initialise any special settings that your component needs.
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 16);
+    slider.setBounds(0, 0, 70, 86);
+    addAndMakeVisible(slider);
+    
+    label.setText(text, juce::NotificationType::dontSendNotification);
+    label.setJustificationType(juce::Justification::horizontallyCentred);
+    label.setBorderSize(juce::BorderSize<int>{ 0, 0, 2, 0 });
+    label.attachToComponent(&slider, false);
+    addAndMakeVisible(label);
+    
+    setSize(70, 110);
 
 }
 
@@ -23,29 +36,7 @@ RotaryKnob::~RotaryKnob()
 {
 }
 
-void RotaryKnob::paint (juce::Graphics& g)
-{
-    /* This demo code just fills the component's background and
-       draws some placeholder text to get you started.
-
-       You should replace everything in this method with your own
-       drawing code..
-    */
-
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));   // clear the background
-
-    g.setColour (juce::Colours::grey);
-    g.drawRect (getLocalBounds(), 1);   // draw an outline around the component
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (14.0f));
-    g.drawText ("RotaryKnob", getLocalBounds(),
-                juce::Justification::centred, true);   // draw some placeholder text
-}
-
 void RotaryKnob::resized()
 {
-    // This method is where you should set the bounds of any child
-    // components that your component contains..
-
+    slider.setTopLeftPosition(0, 24);
 }
