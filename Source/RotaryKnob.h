bfc603e6dd53363020fd9a6257bc77ea7aa3be71
Davis Weimer <davisweimer@gmail.com>
2024-07-23 11:28:10 -0600

    Add args for member init list attachment

--- Source/RotaryKnob.h
+++ Source/RotaryKnob.h
@@ -13,17 +13,24 @@
 #include <JuceHeader.h>
 
 //==============================================================================
-/*
-*/
+
 class RotaryKnob  : public juce::Component
 {
 public:
-    RotaryKnob();
+    RotaryKnob(const juce::String& text,
+               juce::AudioProcessorValueTreeState& apvts,
+               const juce::ParameterID& parameterID);
     ~RotaryKnob() override;
 
-    void paint (juce::Graphics&) override;
     void resized() override;
+    
+    juce::Slider slider;
+    juce::Label label;
+    
+    juce::AudioProcessorValueTreeState::SliderAttachment attachment;
 
 private:
+    
+    //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RotaryKnob)
 };
