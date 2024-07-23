dddf6879c427f59c058a5c0d12a04b8a85792fba
Davis Weimer <davisweimer@gmail.com>
2024-07-23 10:39:20 -0600

    Test general styling of gain param

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -13,9 +13,20 @@
 DelayAudioProcessorEditor::DelayAudioProcessorEditor (DelayAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
+    slider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    slider.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 70, 16);
+    slider.setBounds(0, 0, 70, 86);
+    addAndMakeVisible(slider);
+    
+    label.setText("Gain", juce::NotificationType::dontSendNotification);
+    label.setJustificationType(juce::Justification::horizontallyCentred);
+    label.setBorderSize(juce::BorderSize<int>{ 0, 0, 2, 0 });
+    label.attachToComponent(&slider, false);
+    addAndMakeVisible(label);
+    
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (500, 330);
 }
 
 DelayAudioProcessorEditor::~DelayAudioProcessorEditor()
@@ -25,15 +36,10 @@ DelayAudioProcessorEditor::~DelayAudioProcessorEditor()
 //==============================================================================
 void DelayAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (juce::Colours::beige);
-    g.setColour (juce::Colours::grey);
-    g.setFont (juce::FontOptions (40.0f));
-    g.drawFittedText ("My First Plug-in!", getLocalBounds(), juce::Justification::centredTop, 1);
+    g.fillAll(juce::Colours::darkgrey);
 }
 
 void DelayAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    slider.setTopLeftPosition(215, 120);
 }
