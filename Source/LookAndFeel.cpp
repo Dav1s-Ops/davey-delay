21a19f4a7b5f340ba1a096e67d6f23cd5be649e7
Davis Weimer <davisweimer@gmail.com>
2024-09-14 13:30:58 -0600

    Update knobs w/ gradient bg + drop shadow

--- Source/LookAndFeel.cpp
+++ Source/LookAndFeel.cpp
@@ -17,9 +17,27 @@ RotaryKnobLookAndFeel::RotaryKnobLookAndFeel()
 }
 
 void RotaryKnobLookAndFeel::drawRotarySlider(juce::Graphics& g,
-                                             int x, int y, int width, int height,
+                                             int x, int y, int width, [[maybe_unused]] int height,
                                              float sliderPos, float rotaryStartAngle, float rotaryEndAngle,
                                              juce::Slider& slider)
 {
-//    nothing yet!
+    auto bounds = juce::Rectangle<int>(x, y, width, width).toFloat();
+    auto knobRect = bounds.reduced(10.0f, 10.0f);
+    
+    //==============================================================================
+    // Knob Gradient + Drop Shadow
+    //==============================================================================
+
+    auto path = juce::Path();
+    path.addEllipse(knobRect);
+    dropShadow.drawForPath(g, path);
+    
+    g.setColour(Colors::Knob::outline);
+    g.fillEllipse(knobRect);
+    
+    auto innerRect = knobRect.reduced(3.0f, 3.0f);
+    auto gradient = juce::ColourGradient(Colors::Knob::gradientTop, 0.0f, innerRect.getY(),
+                                         Colors::Knob::gradientBottom, 0.0f, innerRect.getBottom(), false);
+    g.setGradientFill(gradient);
+    g.fillEllipse(innerRect);
 }
