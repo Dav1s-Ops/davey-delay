d5268b0e4867f5cf3201c9a45f0223631794a989
Davis Weimer <davisweimer@gmail.com>
2024-09-29 18:24:16 -0700

    Initialize feedback variables

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -61,6 +61,9 @@ public:
 private:
     Parameters params;
     
+    float feedbackL = 0.0f;
+    float feedbackR = 0.0f;
+    
     juce::dsp::DelayLine<float, juce::dsp::DelayLineInterpolationTypes::Linear> delayLine;
     
     //==============================================================================
